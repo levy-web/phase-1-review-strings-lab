2e08972738b44462f7e0cd1b9e0b911ae2eb357e
levy <lnjiru43@gmail.com>
2022-11-23 13:06:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+let currentUser;
+currentUser = "zevy!"
+
+let welcomeMessage = ("Welcome to Flatbook, "+ currentUser + "!");
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK,  ${currentUser.toUpperCase() } !`;
+
+let shortGreeting = `Welcome, ${currentUser.charAt(0)} !`
+
+console.log(shortGreeting)
+
